c51555235448dbbcd1ba8eb8fcbf28c3ee39a37d
christides11 <christides11@hotmail.com>
2019-11-26 21:57:12 -0500

    Import from project 2, removed tempString.

--- project3.s
+++ project3.s
@@ -6,4 +6,17 @@
 	newLine: .asciiz "\n"
 	userString: .space 1001 #1000 characters
 	charCount: .word 0
-	tempString: .space 1001
+	
+.text # Instructions section, goes in text segment.
+
+main:
+	# PROMPT INPUT #
+	li $v0, 4 # System call to print a string.
+	la $a0, MsgInput # Load string to be printed.
+	syscall # Print string.
+	
+	# READ USER INPUT #
+	li $v0, 8 # System call for taking in input.
+	la $a0, userString # Where the string is saved.
+	li $a1, 1001 # Max number of characters to read.
+	syscall
