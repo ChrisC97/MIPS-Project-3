aa5ca41bb03578f65bd836f41c94fee826f65f8e
christides11 <christides11@hotmail.com>
2019-11-26 21:55:21 -0500

    Imported data from project2, cleaned up variable naming.

---
+++ project3.s
@@ -0,0 +1,9 @@
+.data
+	base: .word 33
+	MsgInput: .asciiz "Input: "
+	MsgInvalid: .asciiz "NaN"
+	MsgDivider: .asciiz ","
+	newLine: .asciiz "\n"
+	userString: .space 1001 #1000 characters
+	charCount: .word 0
+	tempString: .space 1001
